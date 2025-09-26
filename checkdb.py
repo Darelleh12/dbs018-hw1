de7c203e509f80dd1e496e2ac175d7528732fad5
Darelle Herrera <darelleh12@gmail.com>
2025-09-25 22:07:38 -0500

    created checkdb.py

---
+++ checkdb.py
@@ -0,0 +1,8 @@
+
+#Read and execule sql file
+
+#Read and store txt file
+
+#Print referential integrity("Y" or "N") and normalized("Y" or "N")  for each table
+
+#Print referential integrity("Y" or "N") and normalized("Y" or "N")  for the database
