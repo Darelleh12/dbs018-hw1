dace665fc2b0340c443c9eebbe26681e0d559004
Darelle Herrera <darelleh12@gmail.com>
2025-09-25 21:50:25 -0500

    first commiy

---
+++ db_config.py
@@ -0,0 +1,15 @@
+'''
+#Gets exported to hw1.py
+DB_HOST = "54.227.44.99"
+DB_PORT = "5432"
+DB_NAME = "COSC3380"
+DB_USER = "dbs018"
+DB_PASSWORD = "7VioBN9NLJ8u"
+
+'''
+#run locally
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "Meowwoof2!"
